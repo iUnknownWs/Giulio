ec6f135858e54f936075c04ede4ee9656c0008f0
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2024-01-15 15:18:20 -0400

    Hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,31 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#6a012d",
+
+					"secondary": "#4f0020",
+
+					"accent": "#830035",
+
+					"neutral": "#4f0020",
+
+					"base-100": "#fee5f4",
+
+					"info": "#ffffff",
+
+					"success": "#830035",
+
+					"warning": "#ffffff",
+
+					"error": "#ffffff",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
 }
