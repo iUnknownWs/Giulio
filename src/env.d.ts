ae5f4dd2adc97c831a6c5037af6cc1e6e8797b7d
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2024-01-22 17:54:55 -0400

    add blog card

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,19 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export interface BlogPost {
+    id: number;
+    author_details: Details;
+    image_details: Details;
+    title: string;
+    description: string;
+    content: string;
+    publishDate: Date;
+  }
+  
+  export interface Details {
+    id: number;
+    name: string;
+    image: string;
+    uploadDate?: Date;
+  }
