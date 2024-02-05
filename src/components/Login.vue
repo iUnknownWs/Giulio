e324e0f1bbc3f6d275a96e003b6bf86f5e206ccb
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2024-02-05 13:22:46 -0400

    Login validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,73 +1,89 @@
 <template>
-    <div class="flex justify-center items-center h-[785px]">
-        <div class="bg-white shadow-lg rounded p-8 h-min">
-            <h1 class="text-4xl text-primary font-bold text-center my-4 leading-none">
-                LOGIN
-            </h1>
-            <form @submit.prevent="handleSubmit">
-                <div className="max-w-xl mx-auto">
-                    <div class="mb-5">
-                        <label class="form-control w-full max-w-xs">
-                            <div class="label">
-                                <span class="label-text font-medium">Usuario</span>
-                            </div>
-                            <input type="text" placeholder="Introduzca el Usuario"
-                                class="input input-bordered w-full max-w-xs input-primary bg-white" v-model="username"
-                                required />
-                        </label>
-                    </div>
-                    <div class="mb-5">
-                        <label class="form-control w-full max-w-xs">
-                            <div class="label">
-                                <span class="label-text font-medium">Contraseña</span>
-                            </div>
-                            <input type="password" placeholder="Introduzca la contraseña"
-                                class="input input-bordered w-full max-w-xs input-primary bg-white" v-model="password"
-                                required />
-                        </label>
-                    </div>
-                    <div class="flex justify-end mt-8">
-                        <button class="btn btn-active btn-primary text-white">Iniciar Sesion</button>
-                    </div>
-                </div>
-            </form>
+  <div class="flex justify-center items-center h-[785px]">
+    <div class="bg-white shadow-lg rounded p-8 h-min">
+      <h1 class="text-4xl text-primary font-bold text-center my-4 leading-none">
+        LOGIN
+      </h1>
+      <form @submit.prevent="handleSubmit">
+        <div className="max-w-xl mx-auto">
+          <div class="mb-5">
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text font-medium">Usuario</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Introduzca el Usuario"
+                class="input input-bordered w-full max-w-xs input-primary bg-white"
+                v-model="username"
+                required
+              />
+            </label>
+          </div>
+          <div class="mb-5">
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text font-medium">Contraseña</span>
+              </div>
+              <input
+                type="password"
+                placeholder="Introduzca la contraseña"
+                class="input input-bordered w-full max-w-xs input-primary bg-white"
+                v-model="password"
+                required
+              />
+            </label>
+          </div>
+          <div class="flex justify-end mt-8">
+            <button class="btn btn-active btn-primary text-white">
+              Iniciar Sesion
+            </button>
+          </div>
         </div>
+      </form>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'Form',
-    data() {
-        return {
-            username: '',
-            password: '',
-        }
-    },
-    methods: {
-        handleSubmit() {
-            const formData = {
-                username: this.username,
-                password: this.password,
-            };
+  name: "Form",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const formData = {
+        username: this.username,
+        password: this.password,
+      };
 
-            console.log(formData);
+      console.log(formData);
 
-            fetch(`${import.meta.env.PUBLIC_DB}token/`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    localStorage.setItem('token', data.access);
-                    window.location.href = '/';
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
+      fetch(`${import.meta.env.PUBLIC_DB}token/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data.access) {
+            alert("Credenciales incorrectas");
+            location.reload();
+          }
+          if (data.access) {
+            localStorage.setItem("token", data.access);
+            window.location.href = "/";
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-}
+  },
+};
 </script>
